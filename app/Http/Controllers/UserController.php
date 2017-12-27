2d5f59ccd7a2f58a2b07a81db0bdd37a7744661c
chelle tuerk <chelle.tuerk@surveygizmo.com>
2017-12-27 15:58:42 -0700

    users update/put (as a post) is functional, clean up controller files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,55 +2,42 @@
 namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Hash;
 class UserController extends Controller {
 
-	public function index(){
+	public function index() {
 		$users = User::all();
 		return $this->success($users, 200);
 	}
-	public function store(Request $request){
-		$this->validateRequest($request);
+	public function store(Request $request) {
 		$user = User::create([
           'name' => $request->get('name'),
 					'email' => $request->get('email'),
 				]);
 		return $this->success("The user with with id #{$user->id} has been created", 201);
 	}
-	public function show($id){
+	public function show($id) {
 		$user = User::find($id);
 		if(!$user){
 			return $this->error("The user with id #{$id} doesn't exist", 404);
 		}
 		return $this->success($user, 200);
 	}
-	public function update(Request $request, $id){
-		$user = User::find($id);
+	public function update(Request $request) {
+		$user = User::find($request->get('id'));
 		if(!$user){
 			return $this->error("The user with id #{$id} doesn't exist", 404);
 		}
-		$this->validateRequest($request);
-		$user->email 		= $request->get('email');
+		$user->name = $request->get('name');
+		$user->email = $request->get('email');
 		$user->save();
 		return $this->success("The user with with id #{$user->id} has been updated", 200);
 	}
-	public function destroy($id){
+	public function destroy($id) {
 		$user = User::find($id);
 		if(!$user){
 			return $this->error("The user with id #{$id} doesn't exist", 404);
 		}
 		$user->delete();
 		return $this->success("The user with with id #{$id} has been deleted", 200);
-	}
-	public function validateRequest(Request $request){
-		$rules = [
-			'email' => 'required|email|unique:users',
-		];
-		$this->validate($request, $rules);
-	}
-	public function isAuthorized(Request $request){
-		$resource = "users";
-		// $user     = User::find($this->getArgs($request)["user_id"]);
-		return $this->authorizeUser($request, $resource);
 	}
 }
